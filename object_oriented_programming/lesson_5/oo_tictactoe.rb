ea8e96e0aee7a0261767bf0831699f148c43d5c3
Ellery Temple Jr <elreyatee@gmail.com>
2016-02-11 03:43:57 -0800

    changed Player class to Struct

--- object_oriented_programming/lesson_5/oo_tictactoe.rb
+++ object_oriented_programming/lesson_5/oo_tictactoe.rb
@@ -85,13 +85,7 @@ class Square
   end
 end
 
-class Player
-  attr_reader :marker
-
-  def initialize(marker)
-    @marker = marker
-  end
-end
+Player = Struct.new(:marker)
 
 class TTTGame
   HUMAN_MARKER = "X".freeze
