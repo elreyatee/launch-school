e2b9edb6068f38d4631409f85d2faf682ac13bb5
Ellery Temple Jr <elreyatee@gmail.com>
2016-02-15 10:55:43 -0800

    cleanup code a bit

--- challenges/series/series.rb
+++ challenges/series/series.rb
@@ -2,12 +2,11 @@ class Series
   attr_accessor :string
 
   def initialize(string)
-    @string = string
+    @string = string.chars.map(&:to_i)
   end
 
-  def slices(number)
-    raise ArgumentError if number > string.size
-    arr = string.chars.map(&:to_i)
-    arr.each_cons(number).to_a
+  def slices(slice_length)
+    raise ArgumentError if slice_length > string.length
+    string.each_cons(slice_length).to_a
   end
 end
