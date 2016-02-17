fcfceddb46ad32589e6d062df993c5f8f88eab74
Ellery Temple Jr <elreyatee@gmail.com>
2016-02-17 04:40:46 -0800

    completed sieve challenge

--- challenges/sieve/sieve.rb
+++ challenges/sieve/sieve.rb
@@ -1,3 +1,15 @@
-class Sieve 
+class Sieve
+  attr_reader :primes
 
+  def initialize(limit)
+    @primes = []
+
+    range = [*2..limit]
+
+    until range.empty?
+      @primes << range.shift
+      range.delete_if { |n| (n % @primes.last).zero? }
+    end
+  end
 end
+
