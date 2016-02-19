68609f91769ab86231d00b97814b6d27c7d6b8a5
Ellery Temple Jr <elreyatee@gmail.com>
2016-02-19 15:01:25 -0800

    added meetup answer

---
+++ challenges/meetup/meetup.rb
@@ -0,0 +1,21 @@
+require 'date'
+
+class Meetup
+  STARTING_DAYS = { :first => 1, :second => 8, :teenth => 13,
+                    :third => 15, :fourth => 22, :last => -7 }
+
+  def initialize(month, year)
+    @month = month
+    @year = year
+  end
+
+  def day(weekday, schedule)
+    first_day = STARTING_DAYS[schedule]
+    day = (first_day..(first_day + 6)).detect { |day| Date.new(year, month, day).send(weekday.to_s + '?') }
+    @date = Date.new(year, month, day)
+  end
+
+  private
+
+  attr_reader :month, :year, :date
+end
