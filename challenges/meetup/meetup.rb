9a5b1691479ea428c6343872e5082e7fa557c53c
Ellery Temple Jr <elreyatee@gmail.com>
2016-02-19 19:55:48 -0800

    cleaned up code with rubocop

--- challenges/meetup/meetup.rb
+++ challenges/meetup/meetup.rb
@@ -1,8 +1,8 @@
 require 'date'
 
 class Meetup
-  STARTING_DAYS = { :first => 1, :second => 8, :teenth => 13,
-                    :third => 15, :fourth => 22, :last => -7 }
+  STARTING_DAYS = { first: 1, second: 8, teenth: 13,
+                    third: 15, fourth: 22, last: -7 }.freeze
 
   def initialize(month, year)
     @month = month
@@ -11,11 +11,7 @@ class Meetup
 
   def day(weekday, schedule)
     first_day = STARTING_DAYS[schedule]
-    day = (first_day..(first_day + 6)).detect { |day| Date.new(year, month, day).send(weekday.to_s + '?') }
-    @date = Date.new(year, month, day)
+    day = (first_day..(first_day + 6)).detect { |find_day| Date.new(@year, @month, find_day).send(weekday.to_s + '?') }
+    @date = Date.new(@year, @month, day)
   end
-
-  private
-
-  attr_reader :month, :year, :date
 end
