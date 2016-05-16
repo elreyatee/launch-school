d290f6688b65d50b402bbb1373e94df48a71278f
Ellery Temple Jr <elreyatee@gmail.com>
2016-05-16 06:23:22 -0700

    did not declare variables properly ... fixed

--- front-end-dev/javascript/lesson-1-javascript-basics/song.js
+++ front-end-dev/javascript/lesson-1-javascript-basics/song.js
@@ -1,5 +1,5 @@
-word = "bottles";
-count = 99;
+var word = "bottles";
+var count = 99;
 while (count > 0) {
   console.log(count + " " + word + " of beer on the wall");
   console.log(count + " " + word + "of beer,");
