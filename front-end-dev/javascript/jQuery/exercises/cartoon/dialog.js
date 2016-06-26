ea4fc961ae53c571f7a02c7a9cb41acc910731b7
Ellery Temple Jr <elreyatee@gmail.com>
2016-06-25 17:12:39 -0700

    added new project directory

--- front-end-dev/javascript/jQuery/exercises/cartoon/dialog.js
+++ front-end-dev/javascript/jQuery/exercises/cartoon/dialog.js
@@ -3,12 +3,24 @@ $(function() {
       delay = 1500,
       speed = 250;
 
-  $blinds.each(function(index) {
-    var $blind = $blinds.eq(index);
+  function startAnimation() {
+    $blinds.each(function(index) {
+      var $blind = $blinds.eq(index);
 
-    $blind.delay(delay * index + speed).animate({
-      top: "+=" + $(this).height(),
-      height: 0
-    }, speed);
+      $blind.delay(delay * index + speed).animate({
+        top: "+=" + $(this).height(),
+        height: 0
+      }, speed);
+    });
+  }
+
+  $("a").on("click", function(e) {
+    e.preventDefault();
+
+    $blinds.finish();
+    $blinds.removeAttr("style");
+    startAnimation();
   });
+
+  startAnimation();
 });
