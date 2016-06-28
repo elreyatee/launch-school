463b5557ddaf5b96d4705891538589bb498c41b7
Ellery Temple Jr <elreyatee@gmail.com>
2016-06-28 14:38:49 -0700

    modified Game object and added condition for gameover

--- front-end-dev/javascript/jQuery/exercises/guess-a-word/javascripts/game.js
+++ front-end-dev/javascript/jQuery/exercises/guess-a-word/javascripts/game.js
@@ -1,43 +1,46 @@
-var $letters = $("#spaces"),
-    $guesses = $("#guesses"),
-    $apples = $("#apples"),
-    $message = $("#message");
-
 var randomWord = (function() {
-  var words = ["abacus", "quotient", "octothorpe", "proselytize", "stipend"];
+  var words = ["rhythm", "exodus", "voyeurism", "haphazard", "jiujitsu", "kilobyte"];
 
   return function() {
-    var word = words[Math.floor(Math.random() * words.length)];
+    var word = words[Math.floor(Math.random() * word.length)];
     words.splice(words.indexOf(word), 1);
     return word;
   };
 })();
 
+var $message = $("#message"),
+    $spaces  = $("#spaces"),
+    $guesses = $("#guesses"),
+    $apples  = $("#apples");
+
 function Game() {
-  this.incorrect = 0;
+  this.word = randomWord().split(""); // word broken up into letters
   this.letters_guessed = [];
-  this.correct_spaces = 0;
-  this.word = randomWord();
+  this.allowed_wrong_guesses = 6;
+  this.correct_letters = 0;
+  this.incorrect_letters = 0;
+  this.init();
 
   if (!this.word) {
     this.displayMessage("Sorry, I've run out of words!");
   }
-  this.word = this.word.split("");
-  this.init();
 }
 
 Game.prototype = {
   createBlanks: function() {
-    var spaces = (new Array(this.word.length + 1)).join("<span></span>");
+    var blank_spans = "<span></span>".repeat(this.word.length + 1);
 
-    $letters.find("span").remove();
-    $letters.append(spaces);
-    this.$spaces = $("#spaces span");
+    $spaces.find("span").remove();
+    $spaces.append(blank_spans);
   },
-  displayMessage: function(text) {
-    $message.text(text);
+  displayMessage: function(msg) {
+    $message.text(msg);
   },
   init: function() {
     this.createBlanks();
+  },
+  isGameOver: function() {
+    return (this.incorrect_letters === this.allowed_wrong_guesses) ||
+           (this.correct_letters === this.word.length);
   }
-}
+};
