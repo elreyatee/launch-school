a8a99afda7777889308b97a48dc0d9c8cb3c6909
Ellery Temple Jr <elreyatee@gmail.com>
2016-07-13 19:40:12 -0700

    created post object containing title, published date and body. Created Handlebars template to output post properties as a blog post

---
+++ front-end-dev/javascript/jQuery/exercises/handlebars-basics/practice.js
@@ -0,0 +1,7 @@
+$(function() {
+  var post = {
+    title: "Praesent condimentum ligula varius diam.",
+    published: "November 18, 2016",
+    body: "Aenean pretium lectus ut ipsum aliquam, id pharetra elit lobortis. Nulla laoreet justo urna, id vehicula metus aliquet eget. Nunc diam magna, porta ut diam id, venenatis tincidunt arcu. Etiam elementum libero sed nunc porttitor tincidunt. Integer tincidunt neque viverra felis porttitor, in laoreet dui ultrices. Donec sed risus ac sem luctus blandit. Duis dictum congue tellus sit amet rhoncus. Sed posuere, eros a viverra placerat, lorem mi sagittis turpis, sit amet suscipit sapien dui tempor lorem."
+  };
+});
