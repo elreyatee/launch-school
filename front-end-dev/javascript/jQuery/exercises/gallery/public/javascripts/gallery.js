052041c6da4affb62cc210e6cd812ad3799f2951
Ellery Temple Jr <elreyatee@gmail.com>
2016-07-18 20:56:27 -0700

    added photo slide, header and comments on page load for first photo

---
+++ front-end-dev/javascript/jQuery/exercises/gallery/public/javascripts/gallery.js
@@ -0,0 +1,42 @@
+$(function() {
+  var templates = {},
+      photos;
+
+  $("script[type='text/x-handlebars']").each(function() {
+    var $tmpl = $(this);
+    templates[$tmpl.attr("id")] = Handlebars.compile($tmpl.html());
+  });
+
+  $("[data-type=partial]").each(function() {
+    var $partial = $(this);
+    Handlebars.registerPartial($partial.attr("id"), $partial.html());
+  });
+
+  $.ajax({
+    url: "/photos",
+    success: function(json) {
+      photos = json;
+      renderPhotos();
+      renderPhotoInformation(0);
+      getCommentsFor(photos[0].id);
+    }
+  });
+
+  function renderPhotos() {
+    $("#slides").html(templates.photos({ photos: photos }));
+  }
+
+  function renderPhotoInformation(idx) {
+    $("section > header").html(templates.photo_info(photos[idx]));
+  }
+
+  function getCommentsFor(idx) {
+    $.ajax({
+      url: "/comments",
+      data: "photo_id=" + idx,
+      success: function(comment_json) {
+        $("#comments ul").html(templates.comments({ comments: comment_json }));
+      }
+    });
+  }
+});
