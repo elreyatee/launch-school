e30c586520ed75d9db375419a64aa982733aa94f
Ellery Temple Jr <elreyatee@gmail.com>
2016-07-20 07:38:02 -0700

    added prevSlide and nextSlide functionality, hide all slide figures after the first upon first page load

--- front-end-dev/javascript/jQuery/exercises/gallery2/public/javascripts/gallery.js
+++ front-end-dev/javascript/jQuery/exercises/gallery2/public/javascripts/gallery.js
@@ -11,12 +11,42 @@ $(function() {
 		success: function(json) {
 			photos = json;
 			renderPhotos();
+			slideshow.init();
 		}
 	});
 
 	var slideshow = {
-		bind: function() {
+		$slideshow: $("#slideshow"),
+		default: 500,
+		prevSlide: function(e) {
+			e.preventDefault();
 
+			var $current = this.$slideshow.find("figure:visible"),
+			    $prev = $current.prev("figure");
+
+			if (!$prev.length) {
+				$prev = this.$slideshow.find("figure").last();
+			}
+
+			$current.fadeOut(this.default);
+			$prev.fadeIn(this.default);
+		},
+		nextSlide: function(e) {
+			e.preventDefault();
+
+			var $current = this.$slideshow.find("figure:visible"),
+			    $next = $current.next("figure");
+
+			if (!$next.length) {
+				$next = this.$slideshow.find("figure").first();
+			}
+
+			$current.fadeOut(this.default);
+			$next.fadeIn(this.default);
+		},
+		bind: function() {
+			this.$slideshow.find(".prev").on("click", $.proxy(this.prevSlide, this));
+			this.$slideshow.find(".next").on("click", $.proxy(this.nextSlide, this));
 		},
 		init: function() {
 			this.bind();
@@ -24,6 +54,8 @@ $(function() {
 	};
 
 	function renderPhotos() {
+		// render all templates onto page, hide all slide figures after the first
 		$("#slides").html(templates.photos({ photos: photos }));
+		$("#slideshow").find("figure").nextAll().css("display", "none");
 	}
 });
