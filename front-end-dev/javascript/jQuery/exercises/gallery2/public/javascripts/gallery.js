598ed30303612c8267db0a0c260d009c20ad7871
Ellery Temple Jr <elreyatee@gmail.com>
2016-07-21 04:20:32 -0700

    added slideshow and photo content functionality

--- front-end-dev/javascript/jQuery/exercises/gallery2/public/javascripts/gallery.js
+++ front-end-dev/javascript/jQuery/exercises/gallery2/public/javascripts/gallery.js
@@ -2,15 +2,20 @@ $(function() {
 	var templates = {},
 	    photos;
 
+  // capture all templates into an object
 	$("script[type='text/x-handlebars").each(function(_, item) {
 		templates[item.getAttribute("id")] = Handlebars.compile(item.innerHTML);
 	});
+
+	// register partial
+	Handlebars.registerPartial("comment", $("[data-type='partial']").html());
 
 	$.ajax({
 		url: "/photos",
 		success: function(json) {
 			photos = json;
 			renderPhotos();
+			renderPhotoContent(photos[0].id)
 			slideshow.init();
 		}
 	});
@@ -29,7 +34,7 @@ $(function() {
 			}
 
 			$current.fadeOut(this.default);
-			$prev.fadeIn(this.default);
+			$prev.fadeIn(this.default, "linear", renderPhotoContent(+$prev.attr("data-id")));
 		},
 		nextSlide: function(e) {
 			e.preventDefault();
@@ -41,8 +46,8 @@ $(function() {
 				$next = this.$slideshow.find("figure").first();
 			}
 
-			$current.fadeOut(this.default);
-			$next.fadeIn(this.default);
+			$current.fadeOut(this.default, "linear");
+			$next.fadeIn(this.default, "linear", renderPhotoContent(+$next.attr("data-id")));
 		},
 		bind: function() {
 			this.$slideshow.find(".prev").on("click", $.proxy(this.prevSlide, this));
@@ -57,5 +62,28 @@ $(function() {
 		// render all templates onto page, hide all slide figures after the first
 		$("#slides").html(templates.photos({ photos: photos }));
 		$("#slideshow").find("figure").nextAll().css("display", "none");
+	}
+
+	function renderPhotoInformation(photo_id) {
+		var photo = photos.filter(function(item) {
+			return item.id === photo_id;
+		})[0];
+
+		$("section > header").html(templates.photo_information(photo));
+	}
+
+	function renderComments(photo_id) {
+		$.ajax({
+			url: "/comments", 
+			data: "photo_id=" + photo_id,
+			success: function(json) {
+				$("#comments ul").html(templates.comments({ comments: json }));
+			}
+		});
+	}
+
+	function renderPhotoContent(photo_id) {
+		renderPhotoInformation(photo_id);
+		renderComments(photo_id);
 	}
 });
