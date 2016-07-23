1a14431fe59f46d0a9761cde2065aae0f87a3efa
Ellery Temple Jr <elreyatee@gmail.com>
2016-07-23 13:09:22 -0700

    selecting tab now shows associated content

--- front-end-dev/javascript/html5-apis/exercises/localStorage/app.js
+++ front-end-dev/javascript/html5-apis/exercises/localStorage/app.js
@@ -1,5 +1,15 @@
 $(function() {
 	$("nav li").on("mouseenter", function() {
-		$(this).addClass("active").siblings("li").removeClass("active");
+		var $el = $(this),
+		    index = $el.index();
+		    
+		$el.addClass("active").siblings("li").removeClass("active");
+		showContent(index);
 	});
+
+	function showContent(index) {
+		$("#tabs article").each(function(idx, el) {
+			idx === index ? $(el).addClass("selected") : $(el).removeClass("selected");
+		});
+	}
 });
