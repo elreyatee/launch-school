01cd03d16d090e5db2f5d3eaa814d942a2225515
Ellery Temple Jr <elreyatee@gmail.com>
2016-07-23 16:32:47 -0700

    finished assignment

--- front-end-dev/javascript/html5-apis/exercises/localStorage/app.js
+++ front-end-dev/javascript/html5-apis/exercises/localStorage/app.js
@@ -1,25 +1,45 @@
 $(function() {
-	// function showContent(index) {
-	// 	$("#tabs article").each(function(idx, el) {
-	// 		idx === index ? $(el).addClass("selected") : $(el).removeClass("selected");
-	// 	});
-	// }
-
-	// function setActiveTab($tab) {
-	// 	localStorage.setItem("active_tab", JSON.stringify($tab));
-	// }
-
-	// function getActiveTab() {
-	// 	return JSON.parse(localStorage.getItem("active_tab"));
-	// }
-
 	$("nav li").on("click", function() {
 		var $tab = $(this),
 		    class_name = "active",
 		    index = $tab.index();
 
-		$tab.addClass(class_name).siblings("li").removeClass(class_name);
-		// showContent(index);
-		// setActiveTab($tab);
+		$tab.addClass(class_name).siblings().removeClass(class_name);
+		localStorage.setItem("active_nav", index);
+		showArticle(index);
 	});
+
+	$(":radio").on("change", function() {
+		var color = $(this).val();
+
+		$("body").css("background-color", color);
+		localStorage.setItem("background_color", color);
+	});
+
+	$(window).on("unload", function() {
+		localStorage.setItem("note", $("textarea").val());
+	});
+
+	function showArticle(index) {
+		$("article").hide().eq(index).show();
+	}
+
+	function setNote(note) {
+		if (note === null) { return; }
+		$("textarea").val(note);
+	}
+
+	function setBackgroundColor(color) {
+		if (color === null) { return; }
+		$("[value='" + color + "']").prop("checked", true).change();
+	}
+
+	function setActiveNav(index) {
+		if (index === null) { return; }
+		$("nav li").eq(index).click();
+	}
+
+	setActiveNav(localStorage.getItem("active_nav"));
+	setBackgroundColor(localStorage.getItem("background_color"));
+	setNote(localStorage.getItem("note"));
 });
