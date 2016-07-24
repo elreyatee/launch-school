f625eda67d38fae30ad96255b0b26acaba8da6b2
Ellery Temple Jr <elreyatee@gmail.com>
2016-07-23 18:36:39 -0700

    drawn rectangle, circle, and triangle

--- front-end-dev/javascript/html5-apis/exercises/canvas/canvas.js
+++ front-end-dev/javascript/html5-apis/exercises/canvas/canvas.js
@@ -2,16 +2,37 @@ $(function() {
   var canvas = document.querySelector("canvas"),
       ctx = canvas.getContext("2d");
 
-  var colors = ["#000", "#003", "#006", "#009", "#00c", "#00f"];
+  // Draw Rectangle
+  // var colors = ["#000", "#003", "#006", "#009", "#00c", "#00f"];
 
-  function draw() {
-    colors.forEach(function(color, i) {
-      ctx.fillStyle = color;
-      ctx.fillRect(i * 20, i * 20, canvas.width - i * 40, canvas.height - i * 40);
-    });
-    colors.unshift(colors.pop());
-    setInterval(draw, 200);
-  }
+  // function draw() {
+  //   colors.forEach(function(color, i) {
+  //     ctx.fillStyle = color;
+  //     ctx.fillRect(i * 20, i * 20, canvas.width - i * 40, canvas.height - i * 40);
+  //   });
+  //   colors.unshift(colors.pop());
+  //   setInterval(draw, 200);
+  // }
 
-  draw();
+  // draw();
+
+  // Draw Circle
+  var x = canvas.width / 2,
+      y = canvas.height / 2,
+      radius = x;
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.closePath();
+
+  // Draw Triangle
+  ctx.beginPath();
+  ctx.strokeStyle = "rgba(0, 102, 204, .7)";
+  ctx.moveTo(x, y - 50);
+  ctx.lineTo(x + 50, y);
+  ctx.lineTo(x - 50, y);
+  ctx.lineTo(x, y - 50);
+  ctx.stroke();
+  ctx.closePath();
+
 });
