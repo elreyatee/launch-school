214c5c61b55b692902798ab7ff5faa39e4dec271
Ellery Temple Jr <elreyatee@gmail.com>
2016-07-25 05:47:55 -0700

    added clear canvas and drawing all shapes

---
+++ front-end-dev/javascript/html5-apis/exercises/canvas-shape-stamper/canvas.js
@@ -0,0 +1,59 @@
+$(function() {
+  var canvas = document.querySelector("canvas"),
+      ctx = canvas.getContext("2d"),
+      method;
+
+  var drawing_methods = {
+    square: function(e) {
+      var side = 30,
+          x = e.offsetX - side / 2,
+          y = e.offsetY - side / 2;
+
+      ctx.fillRect(x, y, side, side);
+    },
+    circle: function(e) {
+      var radius = 15,
+          x = e.offsetX,
+          y = e.offsetY;
+
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.closePath();
+    },
+    triangle: function(e) {
+      var side = 30,
+          x = e.offsetX,
+          y = e.offsetY - side / 2;
+
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(x + side / 2, y + side);
+      ctx.lineTo(x - side / 2, y + side);
+      ctx.fill();
+      ctx.closePath();
+    }
+  };
+
+  $(".drawing_method").on("click", function(e) {
+    e.preventDefault();
+
+    var $el = $(this);
+
+    $el.closest("ul").find(".active").removeClass("active");
+    $el.addClass("active");
+    method = $el.data("method");
+  }).eq(0).click();
+
+  $("#clear").on("click", function(e) {
+    e.preventDefault();
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  });
+
+  $("canvas").on("click", function(e) {
+    var $el = $(this);
+
+    drawing_methods[method](e);
+  });
+});
