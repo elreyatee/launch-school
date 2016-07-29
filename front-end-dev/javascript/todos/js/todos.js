4762638d3497e65087cb9b08952c17c807e29e8b
Ellery Temple Jr <elreyatee@gmail.com>
2016-07-29 07:32:31 -0700

    changed modal show/hide to javascript

---
+++ front-end-dev/javascript/todos/js/todos.js
@@ -0,0 +1,12 @@
+$(function() {
+  var $modal = $("#modal");
+
+  $("a#add-todo").on("click", function(e) {
+    e.preventDefault();
+    $modal.show();
+  });
+
+  $modal.click(function() {
+    $(this).hide();
+  })
+});
