2a4eb489bd7e7eab5b591b1e2a8b33de60ab319b
Ellery Temple Jr <elreyatee@gmail.com>
2016-07-29 22:30:28 -0700

    added ability to add todo to list

--- front-end-dev/javascript/todos/js/todos.js
+++ front-end-dev/javascript/todos/js/todos.js
@@ -1,5 +1,7 @@
 $(function() {
-  var $modal = $("#modal");
+  var $modal = $("#modal"),
+      $todo_list = $("#todo-list"),
+      todo_items_template = Handlebars.compile($("#list_of_todo_items").html());
 
   $("a#add-todo").on("click", function(e) {
     e.preventDefault();
@@ -10,6 +12,40 @@ $(function() {
     $(this).hide();
   });
 
-  
+  // $(".todo-item").on("click", ".delete", function(e) {
+  //   e.stopPropagation();
+  //
+  //   $(this).closest(".todo-item").remove();
+  // });
 
+  var todos = {
+    collection: [],
+    addTodo: function() {
+      var values = $("#new-todo").serializeArray(),
+          todo_item = {};
+
+      values.forEach(function(field, idx) {
+        todo_item[field.name] = field.value;
+      });
+
+      this.collection.push(todo_item);
+      todo_item["id"] = this.collection.length - 1;
+      return todo_item;
+    },
+    newTodo: function(e) {
+      e.preventDefault();
+
+      var todo_item = this.addTodo();
+
+      $todo_list.append(todo_items_template(todo_item));
+    },
+    bindEvents: function() {
+      $("form#new-todo").on("submit", $.proxy(this.newTodo, this));
+    },
+    init: function() {
+      this.bindEvents();
+    }
+  };
+
+  todos.init();
 });
