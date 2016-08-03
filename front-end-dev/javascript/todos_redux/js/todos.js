ed1b7c819938cb7e98303a37304843bedc291a0a
Ellery Temple Jr <elreyatee@gmail.com>
2016-08-03 01:20:43 -0700

    added ability to delete a list item

--- front-end-dev/javascript/todos_redux/js/todos.js
+++ front-end-dev/javascript/todos_redux/js/todos.js
@@ -1,3 +1,5 @@
+var x;
+
 var all_todos = JSON.parse(localStorage.getItem("all_todos")) || [],
     templates = {};
 
@@ -9,7 +11,7 @@ $(function() {
 
   Todo.prototype = {
     constructor: Todo,
-    todos: all_todos,
+    todos: all_todos || [],
     last_id: localStorage.getItem("id") || 0,
     filterData: function(data) {
       var self = this;
@@ -18,9 +20,21 @@ $(function() {
         self[d.name] = d.value;
       });
     },
+    getTodoByID: function(id) {
+      return this.todos.filter(function(todo) {
+        return todo.id === id;
+      });
+    },
     saveData: function() {
       localStorage.setItem("all_todos", JSON.stringify(this.todos));
       localStorage.setItem("id", this.last_id);
+    },
+    deleteTodo: function(id) {
+      x = this.todos;
+
+      var todo = this.getTodoByID(id);
+      this.todos.splice(todo.id, 1);
+      this.saveData();
     },
     addTodo: function(data) {
       this.last_id++;
@@ -45,12 +59,29 @@ $(function() {
         $tmpl = $(this);
         templates[$tmpl.attr("id")] = Handlebars.compile($tmpl.html());
       });
+
+      $("[data-type='partial']").each(function() {
+        $tmpl = $(this);
+        Handlebars.registerPartial($tmpl.attr("id"), $tmpl.html());
+      });
+    },
+    renderSideBar: function() {
+      $("#sidebar").html(templates.sidebar_tmpl(Todo.prototype));
+    },
+    renderMainPage: function() {
+      $("body").html(templates.main_page(Todo.prototype));
     },
     renderTodoListHeader: function() {
       $("#todo_list_header").html(templates.todo_list_header_tmpl(Todo.prototype));
     },
     renderListItems: function() {
       $("#todo_list_items").html(templates.todo_list_item_tmpl({ todos: Todo.prototype.todos }));
+    },
+    deleteTodo: function(e) {
+      var id = $(e.target).closest("dl").data("id");
+
+      Todo.prototype.deleteTodo(id);
+      this.renderListItems();
     },
     createTodo: function(e) {
       e.preventDefault();
@@ -62,10 +93,12 @@ $(function() {
     bind: function() {
       $("#add_todo img, #modal_background").on("click", form.toggleModal);
       $("form#new_todo").on("submit", $.proxy(this.createTodo, this));
+      $("body").on("click", "button.delete", $.proxy(this.deleteTodo, this));
     },
     init: function() {
       this.loadTemplates();
       this.bind();
+      this.renderMainPage();
     }
   };
 
