01364157aaa2199cd875736e85cb92fadc4d7386
Ellery Temple Jr <elreyatee@gmail.com>
2016-08-03 19:00:32 -0700

    added ability to edit todo

--- front-end-dev/javascript/todos_redux/js/todos.js
+++ front-end-dev/javascript/todos_redux/js/todos.js
@@ -1,5 +1,3 @@
-var x;
-
 var all_todos = JSON.parse(localStorage.getItem("all_todos")) || [],
     templates = {};
 
@@ -13,6 +11,10 @@ $(function() {
     constructor: Todo,
     todos: all_todos || [],
     last_id: localStorage.getItem("id") || 0,
+    default_selection: {
+      title: "All Todos",
+      total: all_todos.length
+    },
     filterData: function(data) {
       var self = this;
 
@@ -29,9 +31,16 @@ $(function() {
       localStorage.setItem("all_todos", JSON.stringify(this.todos));
       localStorage.setItem("id", this.last_id);
     },
-    deleteTodo: function(id) {
-      x = this.todos;
+    updateTodo: function(id, data) {
+      var todo = this.getTodoByID(id)[0];
 
+      data.forEach(function(d) {
+        todo[d.name] = d.value;
+      });
+
+      this.saveData();
+    },
+    deleteTodo: function(id) {
       var todo = this.getTodoByID(id);
       this.todos.splice(todo.id, 1);
       this.saveData();
@@ -46,10 +55,17 @@ $(function() {
 
   var form = {
     data: function() {
-      return $("form#new_todo").serializeArray();
+      return $("form").serializeArray();
     },
     toggleModal: function() {
       $("#modal, #modal_background").fadeToggle();
+    },
+    editForm: function(todo) {
+      this.toggleModal();
+
+      for(var key in todo) {
+        $("[name=" + key + "]").val(todo[key]);
+      }
     }
   };
 
@@ -65,14 +81,11 @@ $(function() {
         Handlebars.registerPartial($tmpl.attr("id"), $tmpl.html());
       });
     },
-    renderSideBar: function() {
-      $("#sidebar").html(templates.sidebar_tmpl(Todo.prototype));
-    },
     renderMainPage: function() {
       $("body").html(templates.main_page(Todo.prototype));
     },
     renderTodoListHeader: function() {
-      $("#todo_list_header").html(templates.todo_list_header_tmpl(Todo.prototype));
+      $("#todo_list_header").html(templates.todo_list_header_tmpl({ total_todos: Todo.prototype.todos.length }));
     },
     renderListItems: function() {
       $("#todo_list_items").html(templates.todo_list_item_tmpl({ todos: Todo.prototype.todos }));
@@ -81,24 +94,61 @@ $(function() {
       var id = $(e.target).closest("dl").data("id");
 
       Todo.prototype.deleteTodo(id);
-      this.renderListItems();
+      this.loadPage();
     },
     createTodo: function(e) {
       e.preventDefault();
 
       form.toggleModal();
       Todo.prototype.addTodo(form.data());
-      this.renderListItems();
+      this.loadPage();
+    },
+    sideBarSelect: function(e) {
+      e.stopPropagation();
+
+      var $item = $(e.target);
+
+      $("#sidebar").find(".selected").removeClass("selected");
+      $item.closest("dl").addClass("selected");
+    },
+    updateTodo: function(todo) {
+      form.editForm(todo);
+
+      $(document).off("submit").on("submit", "form#new_todo", function(e) {
+        e.preventDefault();
+
+        form.toggleModal();
+
+        var form_data = form.data();
+
+        Todo.prototype.updateTodo(todo.id, form_data);
+      });
+    },
+    editTodo: function(e) {
+      e.preventDefault();
+
+      var id = $(e.target).closest("dl").data("id"),
+          todo = Todo.prototype.getTodoByID(id);
+
+      this.updateTodo(todo[0]);
     },
     bind: function() {
-      $("#add_todo img, #modal_background").on("click", form.toggleModal);
-      $("form#new_todo").on("submit", $.proxy(this.createTodo, this));
-      $("body").on("click", "button.delete", $.proxy(this.deleteTodo, this));
+      $(document).on("submit", "form", $.proxy(this.createTodo, this));
+      $(document).on("click", "#add_todo img", form.toggleModal);
+      $(document).on("click", "#modal_background", form.toggleModal);
+      $(document).on("click", "button.delete", $.proxy(this.deleteTodo, this));
+      $(document).on("click", "#all_todos dl", $.proxy(this.sideBarSelect, this));
+      $(document).on("click", "#todo_list_items dl dt", $.proxy(this.editTodo, this));
+    },
+    loadPage: function() {
+      this.renderMainPage();
+      this.renderTodoListHeader();
+      this.renderListItems();
     },
     init: function() {
       this.loadTemplates();
       this.bind();
-      this.renderMainPage();
+      this.loadPage();
     }
   };
 
